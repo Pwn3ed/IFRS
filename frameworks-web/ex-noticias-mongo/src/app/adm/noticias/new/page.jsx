b442f8e20f66a68419550eebca3fb7072e407131
Diego Michel Prestes <diegoprestes99@hotmail.com>
2025-05-02 19:28:40 -0300

    [fix] remove old ex

--- frameworks-web/ex-noticias-mongo/src/app/adm/noticias/new/page.jsx
+++
@@ -1,8 +0,0 @@
-import {gravaNoticia} from '@/lib/noticiasDB'
-import FormEditarNoticia from '@/components/formeditarnoticia'
-
-export default function NoticiasNew(){
-    return(
-        <FormEditarNoticia titulo={''} descricao={''} operacaoNoticia={gravaNoticia} editar={false} />
-    )
-}
--- frameworks-web/ex-noticias-mongo/src/app/noticias/loading.jsx
+++
@@ -1,8 +0,0 @@
-export default async function LoadingNoticias(){
-    return(
-        <div>
-            <h2>Carregando as notícias ...</h2>
-            <h3>Espere só um pouquinho por favor ...</h3>
-        </div>
-    )
-}
